39fb1923fa6b4774e611d000fb183983fa064d41
krithika-h <krithikah15@gmail.com>
2021-03-31 19:32:43 +0530

    Modified

---
+++ Assignments/Section_2/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 5;
+let result;
+
+result = 18 + userInput;
+
+result = result - 5;
+result = result * 5;
+result = result / 3;
+
+alert(result);
+alert(userInput);
---
+++ src/assets/scripts/app.js
@@ -0,0 +1,9 @@
+let currentResult = 0;
+const defaultResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 -1;
+let calculationDescription = `(${defaultResult} + 10) * 
+
+3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
